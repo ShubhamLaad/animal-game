5c8d69dfe172ae6b7ca0b2a7b9e44192e1e1143a
Shubham Laad <shubhamlaad666@gmail.com>
2026-06-06 01:12:53 +0530

    Remove unused style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { App } from './features/App/App';
 import { Provider } from 'react-redux';
@@ -12,7 +11,7 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
